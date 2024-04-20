e8eb1f7c37c4d7a81eaf5374633db0d0736c19b8
bottle-necc <bottlenecc22@gmail.com>
2024-04-21 00:23:51 +0200

    Rewrote a bit of the code to make it more object oriented (even though it would work perfectly fine as regular functions. Allas gotta appeal to project requirements)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.gbxOptions = new System.Windows.Forms.GroupBox();
+            this.nudBlue = new System.Windows.Forms.NumericUpDown();
+            this.nudGreen = new System.Windows.Forms.NumericUpDown();
+            this.nudRed = new System.Windows.Forms.NumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
             this.nudThickness = new System.Windows.Forms.NumericUpDown();
             this.btnRGBSelect = new System.Windows.Forms.Button();
@@ -42,15 +45,12 @@
             this.btnUndo = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
             this.pbxPaper = new System.Windows.Forms.PictureBox();
-            this.nudRed = new System.Windows.Forms.NumericUpDown();
-            this.nudGreen = new System.Windows.Forms.NumericUpDown();
-            this.nudBlue = new System.Windows.Forms.NumericUpDown();
             this.gbxOptions.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBlue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudRed)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudThickness)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbxPaper)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nudRed)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nudGreen)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nudBlue)).BeginInit();
             this.SuspendLayout();
             // 
             // gbxOptions
@@ -72,26 +72,69 @@
             this.gbxOptions.Controls.Add(this.btnUndo);
             this.gbxOptions.Controls.Add(this.btnSave);
             this.gbxOptions.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.gbxOptions.Location = new System.Drawing.Point(-3, -20);
+            this.gbxOptions.Location = new System.Drawing.Point(-2, -16);
+            this.gbxOptions.Margin = new System.Windows.Forms.Padding(2);
             this.gbxOptions.Name = "gbxOptions";
-            this.gbxOptions.Size = new System.Drawing.Size(1005, 148);
+            this.gbxOptions.Padding = new System.Windows.Forms.Padding(2);
+            this.gbxOptions.Size = new System.Drawing.Size(754, 120);
             this.gbxOptions.TabIndex = 0;
             this.gbxOptions.TabStop = false;
+            // 
+            // nudBlue
+            // 
+            this.nudBlue.Location = new System.Drawing.Point(665, 65);
+            this.nudBlue.Margin = new System.Windows.Forms.Padding(2);
+            this.nudBlue.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.nudBlue.Name = "nudBlue";
+            this.nudBlue.Size = new System.Drawing.Size(75, 20);
+            this.nudBlue.TabIndex = 19;
+            // 
+            // nudGreen
+            // 
+            this.nudGreen.Location = new System.Drawing.Point(665, 40);
+            this.nudGreen.Margin = new System.Windows.Forms.Padding(2);
+            this.nudGreen.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.nudGreen.Name = "nudGreen";
+            this.nudGreen.Size = new System.Drawing.Size(75, 20);
+            this.nudGreen.TabIndex = 18;
+            // 
+            // nudRed
+            // 
+            this.nudRed.Location = new System.Drawing.Point(665, 17);
+            this.nudRed.Margin = new System.Windows.Forms.Padding(2);
+            this.nudRed.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.nudRed.Name = "nudRed";
+            this.nudRed.Size = new System.Drawing.Size(75, 20);
+            this.nudRed.TabIndex = 17;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(445, 67);
+            this.label1.Location = new System.Drawing.Point(334, 54);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(69, 16);
+            this.label1.Size = new System.Drawing.Size(56, 13);
             this.label1.TabIndex = 16;
             this.label1.Text = "Thickness";
             // 
             // nudThickness
             // 
-            this.nudThickness.Location = new System.Drawing.Point(436, 107);
+            this.nudThickness.Location = new System.Drawing.Point(327, 87);
+            this.nudThickness.Margin = new System.Windows.Forms.Padding(2);
             this.nudThickness.Name = "nudThickness";
-            this.nudThickness.Size = new System.Drawing.Size(87, 22);
+            this.nudThickness.Size = new System.Drawing.Size(65, 20);
             this.nudThickness.TabIndex = 15;
             this.nudThickness.Value = new decimal(new int[] {
             4,
@@ -102,9 +145,10 @@
             // 
             // btnRGBSelect
             // 
-            this.btnRGBSelect.Location = new System.Drawing.Point(887, 108);
+            this.btnRGBSelect.Location = new System.Drawing.Point(665, 88);
+            this.btnRGBSelect.Margin = new System.Windows.Forms.Padding(2);
             this.btnRGBSelect.Name = "btnRGBSelect";
-            this.btnRGBSelect.Size = new System.Drawing.Size(100, 23);
+            this.btnRGBSelect.Size = new System.Drawing.Size(75, 19);
             this.btnRGBSelect.TabIndex = 13;
             this.btnRGBSelect.Text = "Select";
             this.btnRGBSelect.UseVisualStyleBackColor = true;
@@ -112,45 +156,52 @@
             // 
             // btnText
             // 
-            this.btnText.Location = new System.Drawing.Point(354, 80);
+            this.btnText.Location = new System.Drawing.Point(266, 65);
+            this.btnText.Margin = new System.Windows.Forms.Padding(2);
             this.btnText.Name = "btnText";
-            this.btnText.Size = new System.Drawing.Size(55, 50);
+            this.btnText.Size = new System.Drawing.Size(41, 41);
             this.btnText.TabIndex = 8;
             this.btnText.Text = "Text";
             this.btnText.UseVisualStyleBackColor = true;
             // 
             // btnLine
             // 
-            this.btnLine.Location = new System.Drawing.Point(354, 21);
+            this.btnLine.Location = new System.Drawing.Point(266, 17);
+            this.btnLine.Margin = new System.Windows.Forms.Padding(2);
             this.btnLine.Name = "btnLine";
-            this.btnLine.Size = new System.Drawing.Size(55, 50);
+            this.btnLine.Size = new System.Drawing.Size(41, 41);
             this.btnLine.TabIndex = 7;
             this.btnLine.Text = "Line";
             this.btnLine.UseVisualStyleBackColor = true;
             // 
             // btnSquare
             // 
-            this.btnSquare.Location = new System.Drawing.Point(293, 80);
+            this.btnSquare.Location = new System.Drawing.Point(220, 65);
+            this.btnSquare.Margin = new System.Windows.Forms.Padding(2);
             this.btnSquare.Name = "btnSquare";
-            this.btnSquare.Size = new System.Drawing.Size(55, 50);
+            this.btnSquare.Size = new System.Drawing.Size(41, 41);
             this.btnSquare.TabIndex = 6;
             this.btnSquare.Text = "Square";
             this.btnSquare.UseVisualStyleBackColor = true;
+            this.btnSquare.Click += new System.EventHandler(this.btnSquare_Click);
             // 
             // btnEllips
             // 
-            this.btnEllips.Location = new System.Drawing.Point(293, 21);
+            this.btnEllips.Location = new System.Drawing.Point(220, 17);
+            this.btnEllips.Margin = new System.Windows.Forms.Padding(2);
             this.btnEllips.Name = "btnEllips";
-            this.btnEllips.Size = new System.Drawing.Size(55, 50);
+            this.btnEllips.Size = new System.Drawing.Size(41, 41);
             this.btnEllips.TabIndex = 5;
             this.btnEllips.Text = "Ellipse";
             this.btnEllips.UseVisualStyleBackColor = true;
+            this.btnEllips.Click += new System.EventHandler(this.btnEllips_Click);
             // 
             // btnPen
             // 
-            this.btnPen.Location = new System.Drawing.Point(232, 80);
+            this.btnPen.Location = new System.Drawing.Point(174, 65);
+            this.btnPen.Margin = new System.Windows.Forms.Padding(2);
             this.btnPen.Name = "btnPen";
-            this.btnPen.Size = new System.Drawing.Size(55, 50);
+            this.btnPen.Size = new System.Drawing.Size(41, 41);
             this.btnPen.TabIndex = 4;
             this.btnPen.Text = "Pen";
             this.btnPen.UseVisualStyleBackColor = true;
@@ -158,9 +209,10 @@
             // 
             // btnEraser
             // 
-            this.btnEraser.Location = new System.Drawing.Point(232, 21);
+            this.btnEraser.Location = new System.Drawing.Point(174, 17);
+            this.btnEraser.Margin = new System.Windows.Forms.Padding(2);
             this.btnEraser.Name = "btnEraser";
-            this.btnEraser.Size = new System.Drawing.Size(55, 50);
+            this.btnEraser.Size = new System.Drawing.Size(41, 41);
             this.btnEraser.TabIndex = 3;
             this.btnEraser.Text = "Eraser";
             this.btnEraser.UseVisualStyleBackColor = true;
@@ -168,27 +220,30 @@
             // 
             // btnRedo
             // 
-            this.btnRedo.Location = new System.Drawing.Point(149, 80);
+            this.btnRedo.Location = new System.Drawing.Point(112, 65);
+            this.btnRedo.Margin = new System.Windows.Forms.Padding(2);
             this.btnRedo.Name = "btnRedo";
-            this.btnRedo.Size = new System.Drawing.Size(55, 50);
+            this.btnRedo.Size = new System.Drawing.Size(41, 41);
             this.btnRedo.TabIndex = 2;
             this.btnRedo.Text = "Redo";
             this.btnRedo.UseVisualStyleBackColor = true;
             // 
             // btnUndo
             // 
-            this.btnUndo.Location = new System.Drawing.Point(149, 21);
+            this.btnUndo.Location = new System.Drawing.Point(112, 17);
+            this.btnUndo.Margin = new System.Windows.Forms.Padding(2);
             this.btnUndo.Name = "btnUndo";
-            this.btnUndo.Size = new System.Drawing.Size(55, 50);
+            this.btnUndo.Size = new System.Drawing.Size(41, 41);
             this.btnUndo.TabIndex = 1;
             this.btnUndo.Text = "Undo";
             this.btnUndo.UseVisualStyleBackColor = true;
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(21, 21);
+            this.btnSave.Location = new System.Drawing.Point(16, 17);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(2);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(122, 109);
+            this.btnSave.Size = new System.Drawing.Size(92, 89);
             this.btnSave.TabIndex = 0;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -196,9 +251,10 @@
             // pbxPaper
             // 
             this.pbxPaper.BackColor = System.Drawing.Color.White;
-            this.pbxPaper.Location = new System.Drawing.Point(62, 184);
+            this.pbxPaper.Location = new System.Drawing.Point(46, 150);
+            this.pbxPaper.Margin = new System.Windows.Forms.Padding(2);
             this.pbxPaper.Name = "pbxPaper";
-            this.pbxPaper.Size = new System.Drawing.Size(879, 443);
+            this.pbxPaper.Size = new System.Drawing.Size(659, 360);
             this.pbxPaper.TabIndex = 1;
             this.pbxPaper.TabStop = false;
             this.pbxPaper.Paint += new System.Windows.Forms.PaintEventHandler(this.pbxPaper_Paint);
@@ -206,60 +262,25 @@
             this.pbxPaper.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbxPaper_MouseMove);
             this.pbxPaper.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbxPaper_MouseUp);
             // 
-            // nudRed
-            // 
-            this.nudRed.Location = new System.Drawing.Point(887, 21);
-            this.nudRed.Maximum = new decimal(new int[] {
-            255,
-            0,
-            0,
-            0});
-            this.nudRed.Name = "nudRed";
-            this.nudRed.Size = new System.Drawing.Size(100, 22);
-            this.nudRed.TabIndex = 17;
-            // 
-            // nudGreen
-            // 
-            this.nudGreen.Location = new System.Drawing.Point(887, 49);
-            this.nudGreen.Maximum = new decimal(new int[] {
-            255,
-            0,
-            0,
-            0});
-            this.nudGreen.Name = "nudGreen";
-            this.nudGreen.Size = new System.Drawing.Size(100, 22);
-            this.nudGreen.TabIndex = 18;
-            // 
-            // nudBlue
-            // 
-            this.nudBlue.Location = new System.Drawing.Point(887, 80);
-            this.nudBlue.Maximum = new decimal(new int[] {
-            255,
-            0,
-            0,
-            0});
-            this.nudBlue.Name = "nudBlue";
-            this.nudBlue.Size = new System.Drawing.Size(100, 22);
-            this.nudBlue.TabIndex = 19;
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(996, 687);
+            this.ClientSize = new System.Drawing.Size(747, 558);
             this.Controls.Add(this.pbxPaper);
             this.Controls.Add(this.gbxOptions);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "MS Paint";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.gbxOptions.ResumeLayout(false);
             this.gbxOptions.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBlue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudRed)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudThickness)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbxPaper)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nudRed)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nudGreen)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nudBlue)).EndInit();
             this.ResumeLayout(false);
 
         }
--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,13 +19,16 @@ namespace mspaint
     public partial class Form1 : Form
     {
         private bool isDrawing = false;
-        private Point previousPoint;        // Remembers the previous mouse position.
+        private Point previousPoint;        // Remembers the previous mouse position
         private Color penColor = Color.Black;
         private Color drawColor = Color.Black;
         private int penSize = 4;
         private string selected = "pen";
+        private Draw draw = new Draw();
+        private Square square;
+        private Ellipse ellipse;
 
-        // Bitmap that saves the drawing surface.
+        // Bitmap that saves the drawing surface
         private Bitmap drawingSurface = new Bitmap(800, 600);
 
         public Form1()
@@ -49,39 +53,45 @@ namespace mspaint
             isDrawing = true;
             previousPoint = e.Location;         // Saves the location of the mouse when it began drawing
 
-            if (selected == "square") 
-            { 
-            
-            
+            if (selected == "square")
+            {
+                square = new Square(Graphics.FromImage(drawingSurface), penColor, penSize);
+            }
+            if (selected == "ellipse")
+            {
+                ellipse = new Ellipse(Graphics.FromImage(drawingSurface), penColor, penSize);
             }
         }
 
-        // Runs while the mouse is held, uses the mouse movement to draw.
+        // Runs while the mouse is held, uses the mouse movement to draw
         private void pbxPaper_MouseMove(object sender, MouseEventArgs e)
         {
             if (isDrawing && selected == "pen")
             {
-                using (Graphics g = Graphics.FromImage(drawingSurface))
-                {
-                    Pen pen = new Pen(drawColor, penSize);
-
-                    // Makes the pen a little bit smoother.
-                    g.SmoothingMode = SmoothingMode.HighQuality;
-
-                    // Draws a line from the previous location to the current with the pen.
-                    g.DrawLine(pen, previousPoint, e.Location);
-                }
+                draw.Pen(Graphics.FromImage(drawingSurface), drawColor, penSize, previousPoint, e.Location);
                 previousPoint = e.Location;
 
-                // Updates the picture box to reflect the changes.
+                // Updates the picture box to reflect the changes
                 pbxPaper.Invalidate();
             }
         }
 
-        // Runs when the user releases the mouse button.
+        // Runs when the user releases the mouse button
         private void pbxPaper_MouseUp(object sender, MouseEventArgs e)
         {
             isDrawing = false;
+            if (selected == "square")
+            {
+                square.GetPoints(previousPoint, e.Location);
+                square.Draw();
+                pbxPaper.Invalidate();
+            }
+            if (selected == "ellipse")
+            {
+                ellipse.GetPoints(previousPoint, e.Location);
+                ellipse.Draw();
+                pbxPaper.Invalidate();
+            }
         }
 
         private void pbxPaper_Paint(object sender, PaintEventArgs e)
@@ -95,18 +105,17 @@ namespace mspaint
 
         }
 
-        // Erases by drawing with the color white.
-
+        // Erases by drawing with the color white
         private void btnEraser_Click(object sender, EventArgs e)
         {
-            selected = "pen";
             drawColor = Color.White;
+            selected = "pen";
         }
 
         private void btnPen_Click(object sender, EventArgs e)
         {
-            selected = "pen";
             drawColor = penColor;
+            selected = "pen";
         }
 
         private void nudThickness_ValueChanged(object sender, EventArgs e)
@@ -122,6 +131,147 @@ namespace mspaint
 
             penColor = Color.FromArgb(r, g, b);
             drawColor = penColor;
+        }
+
+        private void btnSquare_Click(object sender, EventArgs e)
+        {
+            selected = "square";
+        }
+
+        private void btnEllips_Click(object sender, EventArgs e)
+        {
+            selected = "ellipse";
+        }
+    }
+
+    public class Draw
+    {
+        public void Pen(Graphics g, Color color, int size, Point previousPoint, Point nextPoint)
+        {
+            Pen pen = new Pen(color, size);
+
+            // Makes the pen a little bit smoother
+            g.SmoothingMode = SmoothingMode.HighQuality;
+
+            // Draws a line from the previous location to the current with the pen
+            g.DrawLine(pen, previousPoint, nextPoint);
+        }
+    }
+
+    // Class containing the common fields and functions of the Square and Ellipse class
+    public class Geometry
+    {
+        protected Graphics _g;
+        protected Color _colour;
+        protected int _size;
+        protected Point _previousPoint;
+        protected Point _nextPoint;
+
+        public Graphics G { get { return _g; } set { _g = value; } }
+        public Color Colour { get { return _colour; } set { _colour = value; } }
+        public int Size { get { return _size; } set { _size = value; } }
+        public Point PreviousPoint { get { return _previousPoint; } set { _previousPoint = value; } }
+        public Point NextPoint { get { return _nextPoint; } set { _nextPoint = value; } }
+
+        public Geometry(Graphics g, Color colour, int size) 
+        {
+            G = g;
+            Colour = colour;
+            Size = size;
+        }
+    }
+
+    public class Square : Geometry
+    {
+        public Square(Graphics g, Color colour, int size) : base(g, colour, size) 
+        { 
+                
+        }
+
+        public void GetPoints(Point previousPoint, Point nextPoint)
+        {
+            PreviousPoint = previousPoint;
+            NextPoint = nextPoint;
+        }
+
+
+        public void Draw()
+        {
+            int oldX = _previousPoint.X;
+            int oldY = _previousPoint.Y;
+            int newX = _nextPoint.X;
+            int newY = _nextPoint.Y;
+
+            // Draws if the mouse moves SE
+            if (newX - oldX > 0 && newY - oldY > 0)
+            {
+                _g.DrawRectangle(new Pen(_colour, _size), oldX, oldY, newX - oldX, newY - oldY);
+            }
+
+            // Draws if the mouse moves NW
+            else if (newX - oldX < 0 && newY - oldY < 0)
+            {
+                _g.DrawRectangle(new Pen(_colour, _size), newX, newY, oldX - newX, oldY - newY);
+            }
+
+            // Draws if the mouse moves NE
+            else if (newX - oldX > 0 &&  newY - oldY < 0)
+            {
+                _g.DrawRectangle(new Pen(_colour, _size), oldX, newY, newX - oldX, oldY - newY);
+            }
+
+            // Draws if the mouse moves SW
+            else if (newX - oldX < 0 && newY - oldY > 0)
+            {
+                _g.DrawRectangle(new Pen(_colour, _size), newX, oldY, oldX - newX, newY - oldY);
+            }
+        }
+    }
+
+    public class Ellipse : Geometry
+    {
+        public Ellipse(Graphics g, Color colour, int size) : base(g, colour, size)
+        {
+
+        }
+
+        public void GetPoints(Point previousPoint, Point nextPoint)
+        {
+            PreviousPoint = previousPoint;
+            NextPoint = nextPoint;
+        }
+
+
+        public void Draw()
+        {
+            int oldX = _previousPoint.X;
+            int oldY = _previousPoint.Y;
+            int newX = _nextPoint.X;
+            int newY = _nextPoint.Y;
+
+            // Draws if the mouse moves SE
+            if (newX - oldX > 0 && newY - oldY > 0)
+            {
+                _g.DrawEllipse(new Pen(_colour, _size), oldX, oldY, newX - oldX, newY - oldY);
+            }
+
+            // Draws if the mouse moves NW
+            else if (newX - oldX < 0 && newY - oldY < 0)
+            {
+                _g.DrawEllipse(new Pen(_colour, _size), newX, newY, oldX - newX, oldY - newY);
+            }
+
+            // Draws if the mouse moves NE
+            else if (newX - oldX > 0 && newY - oldY < 0)
+            {
+                _g.DrawEllipse(new Pen(_colour, _size), oldX, newY, newX - oldX, oldY - newY);
+            }
+
+            // Draws if the mouse moves SW
+            else if (newX - oldX < 0 && newY - oldY > 0)
+            {
+                _g.DrawEllipse(new Pen(_colour, _size), newX, oldY, oldX - newX, newY - oldY);
+            }
         }
     }
 }
